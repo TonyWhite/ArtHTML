b4599315d1036edb1b65188906e609d7afd15df9
TonyWhite <tonywhite1985@gmail.com>
2012-01-07 17:36:58 +0100

    +add GUI

--- ArtHTML/ArtHTML.java
+++ ArtHTML/ArtHTML.java
@@ -4,6 +4,8 @@
  * Autore Bianco Antonio
  * Versione 0.0 alpha
  */
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
 public class ArtHTML
 {
     /**
@@ -28,11 +30,57 @@ public class ArtHTML
                 System.exit(1);
             }
         }
+        else if (args.length==0)
+        {
+            apriMessaggio();
+        }
         else
         {
             System.out.println("Devi passare come parametro il percorso del file da convertire in tabella HTML");
             System.exit(1);
         }
         System.exit(0); // Il programma termina correttamente
+    }
+    
+    private static void apriMessaggio()
+    {
+        final JFileChooser fc = new JFileChooser();
+        int returnVal = fc.showOpenDialog(null);
+        switch (returnVal)
+        {
+            case JFileChooser.APPROVE_OPTION:
+                String errore = "";
+                DisegnaHTML html = new DisegnaHTML();
+                String nomeFile = fc.getSelectedFile().getAbsolutePath();
+                try
+                {
+                    errore = "L'immagine " + nomeFile + " non esiste.";
+                    html.apri(nomeFile);
+                    errore = "Impossibile convertire l'immagine " + nomeFile + ".";
+                    html.converti();
+                }
+                catch(Exception e)
+                {
+                    // Errore di apertura
+                    JOptionPane.showMessageDialog(null, errore, "ArtHTML", JOptionPane.ERROR_MESSAGE);
+                    System.exit(1);
+                }
+                break;
+            
+            case JFileChooser.CANCEL_OPTION:
+                // Apertura annullata
+                System.exit(0);
+                break;
+            
+            case JFileChooser.ERROR_OPTION:
+                // Errore
+                JOptionPane.showMessageDialog(null, "Errore su JFileChooser", "ArtHTML", JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+                break;
+            default:
+                JOptionPane.showMessageDialog(null, "Errore interno: condizione non gestita", "ArtHTML", JOptionPane.ERROR_MESSAGE);
+                System.exit(1);
+                break;
+        }
     }
 }
